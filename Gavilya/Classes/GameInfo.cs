23ff2e3ad88d057629598009b6ea4b499c3eb64e
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-08-23 11:38:26 +0200

    Added the "GameInfo" class

---
+++ Gavilya/Classes/GameInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Gavilya.Classes
+{
+    /// <summary>
+    /// Informations that a "Game" countains.
+    /// </summary>
+    public class GameInfo
+    {
+        /// <summary>
+        /// Name of the game.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The location of the game.
+        /// </summary>
+        public string FileLocation { get; set; }
+
+        /// <summary>
+        /// The version of the game.
+        /// </summary>
+        public string Version { get; set; }
+
+        /// <summary>
+        /// The location of the Icon of the game.
+        /// </summary>
+        public string IconFileLocation { get; set; }
+
+        /// <summary>
+        /// The icon of the game.
+        /// </summary>
+        public Image Icon { get; set; }
+    }
+}
