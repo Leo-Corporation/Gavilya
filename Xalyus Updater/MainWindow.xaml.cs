7a504329e02962ebf3e97469d089487c9778fc48
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-09-11 18:41:22 +0200

    Add an updater (#15)

--- Xalyus Updater/MainWindow.xaml.cs
+++ Xalyus Updater/MainWindow.xaml.cs
@@ -23,8 +23,11 @@ SOFTWARE.
 */
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -43,14 +46,75 @@ namespace Xalyus_Updater
     /// </summary>
     public partial class MainWindow : Window
     {
+        WebClient client = new WebClient(); // Webclient
         public MainWindow()
         {
             InitializeComponent();
+            InitText(); // Load the text
         }
 
         private void CloseBtn_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void InitText()
+        {
+            switch (Thread.CurrentThread.CurrentUICulture.Name) // In each case for the language name
+            {
+                case "fr-FR": // If the language is French
+                    TitleTxt.Text = "Gavilya"; // Title
+                    DescriptionTxt.Text = "Gavilya est un simple lanceur de jeu."; // Description
+                    break;
+                case "en-US": // If the language is English
+                    TitleTxt.Text = "Gavilya"; // Title
+                    DescriptionTxt.Text = "Gavilya is a simple a game launcher."; // Description
+                    break;
+
+                default: // Default
+                    TitleTxt.Text = "Gavilya"; // Title
+                    DescriptionTxt.Text = "Gavilya is a simple a game launcher."; // Description
+                    break;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            WebClient linkDownloader = new WebClient(); // Link downloader
+            string link = linkDownloader.DownloadString(Global.ZIPLink); // Download the link
+
+            client.DownloadProgressChanged += Client_DownloadProgressChanged; // Register event
+            client.DownloadFileCompleted += Client_DownloadFileCompleted; // Register event
+
+            if (!string.IsNullOrEmpty(link))
+            {
+                Thread thread = new Thread(() =>
+                {
+                    Uri uri = new Uri(link);
+                    client.DownloadFileAsync(uri, Global.Directory); // Download
+                });
+                thread.Start();
+            }
+        }
+
+        private void Client_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                Close();
+            });
+        }
+
+        private void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                double receive = double.Parse(e.BytesReceived.ToString()); // Total downloaded
+                double total = double.Parse(e.TotalBytesToReceive.ToString()); // Total
+                double percentage = receive / total * 100; // Calculate the percentage
+                ProgressTxt.Text = $"{string.Format("{0:0.##}", percentage)}%"; // Show the progress
+                Pgb.Value = int.Parse(Math.Truncate(percentage).ToString()); // Update the progress bar value
+            });
         }
     }
 }
