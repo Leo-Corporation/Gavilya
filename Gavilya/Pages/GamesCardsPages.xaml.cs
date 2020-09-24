0e07e4ba70acda7d82bdc2f7bd060be791bc5ca1
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-09-24 18:31:44 +0200

    Added Drag and Drop

--- Gavilya/Pages/GamesCardsPages.xaml.cs
+++ Gavilya/Pages/GamesCardsPages.xaml.cs
@@ -23,6 +23,7 @@ SOFTWARE.
 */
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Threading;
@@ -38,6 +39,7 @@ using System.Windows.Shapes;
 using Gavilya.Classes;
 using Gavilya.UserControls;
 using Gavilya.Windows;
+using LeoCorpLibrary;
 
 namespace Gavilya.Pages
 {
@@ -55,6 +57,41 @@ namespace Gavilya.Pages
         private void AddBtn_Click(object sender, RoutedEventArgs e)
         {
             new AddGame().Show(); // Open the "Add Game" dialog
+        }
+
+        private void GamePresenter_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[]) e.Data.GetData(DataFormats.FileDrop); // Get all the files droped
+                List<string> executables = new List<string>(); // The execuables files
+
+                for (int i = 0; i < files.Length; i++) // For each file
+                {
+                    if (System.IO.Path.GetExtension(files[i]) == ".exe") // If the file is a .exe
+                    {
+                        executables.Add(files[i]); // Add the file to the executables
+                    }
+                }
+
+                for (int i = 0; i < executables.Count; i++) // For each executables (or games)
+                {
+                    FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(executables[i]);
+                    GameInfo gameInfo = new GameInfo // Create a new GameInfo class/object
+                    {
+                        FileLocation = executables[i],
+                        IsFavorite = false,
+                        Name = string.IsNullOrEmpty(fileVersionInfo.ProductName) ? System.IO.Path.GetFileNameWithoutExtension(executables[i]) : fileVersionInfo.ProductName,
+                        LastTimePlayed = 0,
+                        TotalTimePlayed = 0,
+                        IconFileLocation = string.Empty,
+                        Version = fileVersionInfo.FileVersion // Get the version
+                    };
+                    Definitions.Games.Add(gameInfo); // Add the games to the List<GameInfo>
+                    Definitions.GamesCardsPages.GamePresenter.Children.Add(new GameCard(gameInfo)); // Add the games to the GamePresenter
+                    new GameSaver().Save(Definitions.Games); // Save the added games
+                }
+            }
         }
     }
 }
