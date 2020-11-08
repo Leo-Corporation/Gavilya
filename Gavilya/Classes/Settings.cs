fed38f5ebf53a4a7e800022f73a8fcfe446d664d
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-11-08 14:20:54 +0100

    Create Settings.cs

---
+++ Gavilya/Classes/Settings.cs
@@ -0,0 +1,87 @@
+﻿/*
+MIT License
+
+Copyright (c) Léo Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. 
+*/
+using LeoCorpLibrary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Gavilya.Classes
+{
+    /// <summary>
+    /// Settings of Gavilya
+    /// </summary>
+    public class Settings
+    {
+        /// <summary>
+        /// The language of Gavilya.
+        /// </summary>
+        public string Language { get; set; }
+    }
+
+    public static class SettingsSaver
+    {
+        /// <summary>
+        /// Loads the settings into <see cref="Definitions.Settings"/>.
+        /// </summary>
+        public static void Load()
+        {
+            string AppDataPath = Env.GetAppDataPath(); // Get %APPDATA% folder
+            if (File.Exists(AppDataPath + @"\Gavilya\Settings.gavsettings"))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Settings)); // XML Serializer
+                StreamReader streamReader = new StreamReader(AppDataPath + @"\Gavilya\Settings.gavsettings"); // Where the file is going to be read
+
+                Definitions.Settings = (Settings)xmlSerializer.Deserialize(streamReader); // Read
+
+                streamReader.Dispose();
+            }
+            else
+            {
+                Definitions.Settings = new Settings { Language = "_default" };
+                Save();
+            }
+        }
+
+        /// <summary>
+        /// Saves the settings into the <c>%APPDATA%</c> folder.
+        /// </summary>
+        public static void Save()
+        {
+            string AppDataPath = Env.GetAppDataPath(); // Get %APPDATA% folder
+            XmlSerializer xmlSerializer = new XmlSerializer(Definitions.Settings.GetType()); // XML Serializer
+
+            if (!Directory.Exists(AppDataPath + @"\Gavilya")) // If the directory doesn't exist
+            {
+                Directory.CreateDirectory(AppDataPath + @"\Gavilya"); // Create the directory
+            }
+
+            StreamWriter streamWriter = new StreamWriter(AppDataPath + @"\Gavilya\Settings.gavsettings"); // The place where the file is going to be written
+            xmlSerializer.Serialize(streamWriter, Definitions.Settings);
+
+            streamWriter.Dispose();
+        }
+    }
+}
