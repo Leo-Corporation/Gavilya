fd0f13214f7f9c4c754ccf068d42e99b047cf14d
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-06-06 13:37:50 +0200

    Updated assets of "First run" and more (#97)

--- Gavilya/Classes/Definitions.cs
+++ Gavilya/Classes/Definitions.cs
@@ -100,7 +100,7 @@ namespace Gavilya.Classes
 		/// <summary>
 		/// Define if the current build is a preversion of Gavilya, not made for production nor Pre-Release.
 		/// </summary>
-		public static bool IsBeta => false;
+		public static bool IsBeta => true;
 
 
 		/// <summary>
