451e914c30fdf5764531d86c56a923db67902bc0
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-08-06 11:47:55 +0200

    Version 1.8.0.2108-pre1

--- Gavilya/Classes/Definitions.cs
+++ Gavilya/Classes/Definitions.cs
@@ -81,7 +81,7 @@ namespace Gavilya.Classes
 		/// <summary>
 		/// Version of the software (Gavilya).
 		/// </summary>
-		public static string Version => "1.7.0.2107";
+		public static string Version => "1.8.0.2108-pre1";
 
 		/// <summary>
 		/// True if the menu is shown.
