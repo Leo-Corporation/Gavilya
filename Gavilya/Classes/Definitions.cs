548d6cdad9809bd3ce58f945b109f68d6b71a24f
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-10-26 11:13:29 +0200

    Version 2.0.0.2110-pre4

--- Gavilya/Classes/Definitions.cs
+++ Gavilya/Classes/Definitions.cs
@@ -78,7 +78,7 @@ namespace Gavilya.Classes
 		/// <summary>
 		/// Version of the software (Gavilya).
 		/// </summary>
-		public static string Version => "2.0.0.2110-pre3";
+		public static string Version => "2.0.0.2110-pre4";
 
 		/// <summary>
 		/// True if the menu is shown.
