e9d1a4ed0ddd8bc6fce6fe08c8b7af5a4b93ca5d
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-11-22 13:35:14 +0100

    Fixed: "Popup menu" window doesn't have a name (#159)

--- Gavilya/Properties/Resources.Designer.cs
+++ Gavilya/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Gavilya.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -1256,6 +1256,15 @@ namespace Gavilya.Properties {
         public static string PlayTo {
             get {
                 return ResourceManager.GetString("PlayTo", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Gavilya - Menu.
+        /// </summary>
+        public static string PopupMenu {
+            get {
+                return ResourceManager.GetString("PopupMenu", resourceCulture);
             }
         }
         
