5facac46f061e7be531f5876996bb36a5a1cc435
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-11-24 15:12:48 +0100

    Version 2.1.0.2111-rc1

--- Gavilya/Classes/Definitions.cs
+++ Gavilya/Classes/Definitions.cs
@@ -78,7 +78,7 @@ namespace Gavilya.Classes
 		/// <summary>
 		/// Version of the software (Gavilya).
 		/// </summary>
-		public static string Version => "2.1.0.2111-pre1";
+		public static string Version => "2.1.0.2111-rc1";
 
 		/// <summary>
 		/// True if the menu is shown.
