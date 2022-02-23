d3227253b651fc2f39e631e5ad9f3e9b1932d2ae
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-02-23 12:52:03 +0100

    Version 2.3.1.2202

--- Gavilya/Classes/Definitions.cs
+++ Gavilya/Classes/Definitions.cs
@@ -78,7 +78,7 @@ public static class Definitions
 	/// <summary>
 	/// Version of the software (Gavilya).
 	/// </summary>
-	public static string Version => "2.3.0.2201";
+	public static string Version => "2.3.1.2202";
 
 	/// <summary>
 	/// True if the menu is shown.
