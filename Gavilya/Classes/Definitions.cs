e48cc9455e612960d47be39b80fb7aafa281ba0c
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-07-12 10:21:20 +0200

    Version 2.7.0.2207-rc1

--- Gavilya/Classes/Definitions.cs
+++ Gavilya/Classes/Definitions.cs
@@ -52,7 +52,7 @@ public static class Definitions
 	/// <summary>
 	/// Version of the software (Gavilya).
 	/// </summary>
-	public static string Version => "2.7.0.2207-pre1";
+	public static string Version => "2.7.0.2207-rc1";
 
 	/// <summary>
 	/// True if the menu is shown.
