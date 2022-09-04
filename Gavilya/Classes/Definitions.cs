b47ffcd4ca8018b17b7b72abcd2ad35cb114a4ba
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-09-04 10:48:44 +0200

    Version 2.9.0.2209

--- Gavilya/Classes/Definitions.cs
+++ Gavilya/Classes/Definitions.cs
@@ -52,7 +52,7 @@ public static class Definitions
 	/// <summary>
 	/// Version of the software (Gavilya).
 	/// </summary>
-	public static string Version => "2.9.0.2209-rc1";
+	public static string Version => "2.9.0.2209";
 
 	/// <summary>
 	/// True if the menu is shown.
