9cfd62c921906e815c06e99796254e7b9ad6b64b
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-11-07 09:19:27 +0100

    Version 3.0.1.2211

--- Gavilya/Classes/Definitions.cs
+++ Gavilya/Classes/Definitions.cs
@@ -52,7 +52,7 @@ public static class Definitions
 	/// <summary>
 	/// Version of the software (Gavilya).
 	/// </summary>
-	public static string Version => "3.0.0.2210";
+	public static string Version => "3.0.1.2211";
 
 	/// <summary>
 	/// True if the Fps Counter is visible. (default: false)
