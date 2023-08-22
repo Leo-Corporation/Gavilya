9e0b1fcd39bda7423f714836f8d43dc94016ff26
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-08-22 11:21:09 +0200

    Added basic models (#396)

---
+++ Gavilya/Enums/GameType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gavilya.Enums;
+public enum GameType
+{
+	Win32,
+	Steam,
+	UWP
+}
