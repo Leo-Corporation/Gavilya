935131d43644c38c399112d206fc54ad052f7a2b
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-08-22 12:52:34 +0200

    Added NavBar (#397)

---
+++ Gavilya/ViewModels/NavBarViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Gavilya.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Gavilya.ViewModels;
+public class NavBarViewModel : ViewModelBase
+{
+    public ICommand HomePageCommand { get; }
+    public NavBarViewModel()
+    {
+        HomePageCommand = new RelayCommand(HomePage);
+    }
+
+    private void HomePage(object? obj)
+    {
+        
+    }
+}
