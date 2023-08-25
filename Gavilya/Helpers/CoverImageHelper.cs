a7b4a7086dc08553444fd625e00a378434f4e296
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-08-25 13:36:07 +0200

    Added CoverImageHelper (#399)

---
+++ Gavilya/Helpers/CoverImageHelper.cs
@@ -0,0 +1,85 @@
+﻿/*
+MIT License
+
+Copyright (c) Léo Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. 
+*/
+
+using Gavilya.Models;
+using Gavilya.Models.Rawg;
+using PeyrSharp.Env;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Gavilya.Helpers
+{
+    public class CoverImageHelper
+    {
+        private string _url;
+        private int _id;
+		private readonly int _gameId;
+
+		public CoverImageHelper(string url, int id, int gameId)
+        {
+            _url = url;
+			_id = id;
+			_gameId = gameId;
+		}
+
+        public async Task<string> Download()
+        {
+
+			if (!Directory.Exists(FileSys.AppDataPath + @"\Léo Corporation\Gavilya\Games")) // If the directory doesn't exist
+			{
+				Directory.CreateDirectory(FileSys.AppDataPath + @"\Léo Corporation\Gavilya\Games"); // Create the direspctory
+			}
+
+			if (!Directory.Exists(FileSys.AppDataPath + @$"\Léo Corporation\Gavilya\games\{_gameId}")) // If the directory doesn't exist
+			{
+				Directory.CreateDirectory(FileSys.AppDataPath + $@"\Léo Corporation\Gavilya\Games\{_gameId}"); // Create the game directory
+			}
+			else
+			{
+				if (File.Exists(FileSys.AppDataPath + $@"\Léo Corporation\Gavilya\Games\{_gameId}\bg_img{_id}.jpg")) // If the image exist
+				{
+					File.Delete(FileSys.AppDataPath + $@"\Léo Corporation\Gavilya\Games\{_gameId}\bg_img{_id}.jpg");
+				}
+				await DownloadFileAsync(new Uri(_url), FileSys.AppDataPath + $@"\Léo Corporation\Gavilya\Games\{_gameId}\bg_img{_id}.jpg"); // Download the image
+
+				return FileSys.AppDataPath + @$"\Léo Corporation\Gavilya\Games\{_gameId}\bg_img{_id}.jpg"; // Return the result
+			}
+			await DownloadFileAsync(new Uri(_url), FileSys.AppDataPath + $@"\Léo Corporation\Gavilya\Games\{_gameId}\bg_img{_id}.jpg"); // Download the image
+			return FileSys.AppDataPath + @$"\Léo Corporation\Gavilya\Games\{_gameId}\bg_img{_id}.jpg"; // Return the path
+		}
+
+		private async Task DownloadFileAsync(Uri uri, string filePath)
+		{
+			using var s = await new HttpClient().GetStreamAsync(uri);
+			using var fs = new FileStream(filePath, FileMode.CreateNew);
+			await s.CopyToAsync(fs);
+		}
+    }
+}
