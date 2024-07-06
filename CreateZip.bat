d99377f6595b897084b7cc1b053ccac21c2e5ccb
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-07-06 11:30:21 +0200

    Updated Create Zip script

--- CreateZip.bat
+++ CreateZip.bat
@@ -1,7 +1,7 @@
 @echo off
 mkdir Zip
 
-xcopy Gavilya\bin\Release\net6.0-windows\ Zip /s /e /y
+xcopy Gavilya\bin\Release\net8.0-windows\ Zip /s /e /y
 xcopy Gavilya.Fps\bin\Release\ Zip /s /e /y
 
 pause
