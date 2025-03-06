c660d4b93ebe676e80d871c4f97b2e6e5270b0d7
Léo Peyronnet <leo.peyronnet@outlook.fr>
2025-03-06 10:59:29 +0100

    Added new About page (#476)

--- Gavilya/ViewModels/Settings/AboutViewModel.cs
+++ Gavilya/ViewModels/Settings/AboutViewModel.cs
@@ -27,6 +27,7 @@ using Gavilya.Helpers;
 using Gavilya.Models;
 using PeyrSharp.Core;
 using PeyrSharp.Env;
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.Windows;
@@ -47,8 +48,24 @@ public class AboutViewModel : ViewModelBase
 	private string _statusIcon = "\uF299";
 	public string StatusIcon { get => _statusIcon; set { _statusIcon = value; OnPropertyChanged(nameof(StatusIcon)); } }
 
-	private SolidColorBrush _iconColor = new() { Color = Color.FromRgb(37, 222, 15) };
-	public SolidColorBrush IconColor { get => _iconColor; set { _iconColor = value; OnPropertyChanged(nameof(IconColor)); } }
+	private string _checkUpdateBtnContent = "\uF191";
+	public string CheckUpdateBtnContent { get => _checkUpdateBtnContent; set { _checkUpdateBtnContent = value; OnPropertyChanged(nameof(CheckUpdateBtnContent)); } }
+
+	private FontFamily _iconFont = new(new Uri("pack://application:,,,/"), "./Fonts/#FluentSystemIcons-Regular");
+	public FontFamily IconFont { get => _iconFont; set { _iconFont = value; OnPropertyChanged(nameof(IconFont)); } }
+
+	private int _iconSize = 14;
+	public int IconSize { get => _iconSize; set { _iconSize = value; OnPropertyChanged(nameof(IconSize)); } }
+
+	private FontWeight _fontWeight = FontWeights.Normal;
+	public FontWeight FontWeight { get => _fontWeight; set { _fontWeight = value; OnPropertyChanged(nameof(FontWeight)); } }
+
+	private SolidColorBrush _foregroundColor = ThemeHelper.GetSolidColor("ForegroundGreen");
+	public SolidColorBrush ForegroundColor { get => _foregroundColor; set { _foregroundColor = value; OnPropertyChanged(nameof(ForegroundColor)); } }
+
+	private SolidColorBrush _backgroundColor = ThemeHelper.GetSolidColor("LightGreen");
+	public SolidColorBrush BackgroundColor { get => _backgroundColor; set { _backgroundColor = value; OnPropertyChanged(nameof(BackgroundColor)); } }
+
 
 	public ICommand UpdateCommand { get; }
 	public ICommand LicensesCommand { get; }
@@ -73,7 +90,12 @@ public class AboutViewModel : ViewModelBase
 			{
 				StatusMessage = Properties.Resources.UpdateAv;
 				StatusIcon = "\uF36E";
-				IconColor = new() { Color = Color.FromRgb(255, 50, 30) };
+				ForegroundColor = ThemeHelper.GetSolidColor("ForegroundOrange");
+				BackgroundColor = ThemeHelper.GetSolidColor("LightOrange");
+				CheckUpdateBtnContent = Properties.Resources.Install;
+				IconFont = new FontFamily(new Uri("pack://application:,,,/"), "./Fonts/#Hauora");
+				IconSize = 12;
+				FontWeight = FontWeights.Bold;
 
 				if (MessageBox.Show($"{Properties.Resources.UpdateAvMessage}\n{Properties.Resources.UpdateVersion} {lastVer}\n\n{Properties.Resources.ContinueInstall}", $"{Properties.Resources.Version} {lastVer}", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.No)
 				{
@@ -87,7 +109,12 @@ public class AboutViewModel : ViewModelBase
 
 			StatusMessage = Properties.Resources.UpdateUn;
 			StatusIcon = "\uF299";
-			IconColor = new() { Color = Color.FromRgb(37, 222, 15) };
+			ForegroundColor = ThemeHelper.GetSolidColor("ForegroundGreen");
+			BackgroundColor = ThemeHelper.GetSolidColor("LightGreen");
+			CheckUpdateBtnContent = "\uF191";
+			IconFont = new FontFamily(new Uri("pack://application:,,,/"), "./Fonts/#FluentSystemIcons-Regular");
+			IconSize = 14;
+			FontWeight = FontWeights.Normal;
 		}
 	}
 
